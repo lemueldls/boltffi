27b5a272464c52cd5b35fc3b81dd741095e2fb1f
Ali <ali94_200970@yahoo.com>
2025-06-22 10:52:00 +0200

    first version exports

--- mobiFFI_core/src/lib.rs
+++ mobiFFI_core/src/lib.rs
@@ -1,1 +1,20 @@
 #![allow(unused)]
+
+pub const VERSION_MAJOR: u32 = 0;
+pub const VERSION_MINOR: u32 = 1;
+pub const VERSION_PATCH: u32 = 0;
+
+#[unsafe(no_mangle)]
+pub extern "C" fn mffi_version_major() -> u32 {
+    VERSION_MAJOR
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn mffi_version_minor() -> u32 {
+    VERSION_MINOR
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn mffi_version_patch() -> u32 {
+    VERSION_PATCH
+}
