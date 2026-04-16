4f302c927c8195b30e10b87f73eda9aa70df8e05
Tymon <ammar.hilal71@gmail.com>
2026-04-16 20:48:26 +0200

    improve string test cases

--- examples/platforms/python/tests/primitives/test_strings.py
+++ examples/platforms/python/tests/primitives/test_strings.py
@@ -5,12 +5,21 @@ import demo
 
 class StringsTests(unittest.TestCase):
     def test_echo_string(self) -> None:
-        self.assertEqual(demo.echo_string("hello 🌍"), "hello 🌍")
+        self.assertEqual(demo.echo_string("hello"), "hello")
+        self.assertEqual(demo.echo_string(""), "")
+        self.assertEqual(demo.echo_string("café"), "café")
+        self.assertEqual(demo.echo_string("日本語"), "日本語")
+        self.assertEqual(demo.echo_string("hello 🌍 world"), "hello 🌍 world")
 
     def test_concat_strings(self) -> None:
         self.assertEqual(demo.concat_strings("foo", "bar"), "foobar")
+        self.assertEqual(demo.concat_strings("", "bar"), "bar")
+        self.assertEqual(demo.concat_strings("foo", ""), "foo")
+        self.assertEqual(demo.concat_strings("🎉", "🎊"), "🎉🎊")
 
     def test_string_length(self) -> None:
+        self.assertEqual(demo.string_length("hello"), 5)
+        self.assertEqual(demo.string_length(""), 0)
         self.assertEqual(demo.string_length("café"), 5)
         self.assertEqual(demo.string_length("🌍"), 4)
 
